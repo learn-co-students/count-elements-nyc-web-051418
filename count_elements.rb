e53c0ae46bf5aeb684bbda31b1dcb20106b51f3d
JohnZBurnett <tuninator@gmail.com>
2018-05-14 16:52:47 -0400

    write count method

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,13 @@
 def count_elements(array)
   # code goes here
+  count_hash = {}
+  array.each do |item|
+    if !count_hash[item]
+      count_hash[item] = 1
+    else
+      count_hash[item] += 1
+    end
+  end 
+
+    count_hash
 end
- 

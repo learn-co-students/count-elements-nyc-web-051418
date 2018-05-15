63d8f9bcd445f56b2436451d62614e813cb77746
David Tenenbaum <github email address>
2018-05-14 22:51:04 -0400

    Done

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,8 @@
 def count_elements(array)
   # code goes here
+  new_hash = {}
+    array.each do |element|
+      new_hash.has_key?(element) ? new_hash[element] += 1 : new_hash[element] = 1
+    end
+  new_hash
 end
- 
